4d252173ad49452d478d2b91f1f42ef90677b9c9
im-jking <jamesianking@hotmail.com>
2023-02-22 23:04:47 -0800

    Declared chunkTime and fixed heroPos

--- Assets/src/Ian/Scripts/LoadLevel.cs
+++ Assets/src/Ian/Scripts/LoadLevel.cs
@@ -4,11 +4,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LoadLevel : MonoBehaviour{
-
+    private float chunkTime;
 
     //Create timer to check whether a chunk has been loaded in the past two seconds. This prevents framerate-dependent chunk loading.
     void Start(){
-        float chunkTime = 0.0f;
+        chunkTime = 0.0f;
     }
 
     //Subtract the seconds since last fram from the chunk loading timer. If this puts the timer below 0, set it to 0.
@@ -23,11 +23,11 @@ public class LoadLevel : MonoBehaviour{
 
     /*  This function will spawn the first three chunks of the level immediately. It will then check whether 
         the hero has reached the end of a chunk and, if so, spawn another chunk past the current final chunk. */
-    void CreateNewChunk(Vector2 heroPos) {
-        if(heroPos.position.x < 10){
-            Debug.Log("Ian - Load first three chunks at " + heroPos.position.x + ", " + (heroPos.position.x + 25) + ", and " + (heroPos.position.x + 50));
-        } else if(((heroPos.position.x) % 25) < .1 && chunkTime){ 
-            Debug.Log("Ian - Load next chunk in level at x-value " + heroPos.position.x +" and y-value " + heroPos.position.y);
+    void CreateNewChunk(Vector3 heroPos) {
+        if(heroPos.x < 10){
+            Debug.Log("Ian - Load first three chunks at " + heroPos.x + ", " + (heroPos.x + 25) + ", and " + (heroPos.x + 50));
+        } else if(((heroPos.x) % 25) < .1 && chunkTime){ 
+            Debug.Log("Ian - Load next chunk in level at x-value " + heroPos.x +" and y-value " + heroPos.y);
             chunkTime = 2;
         }
     }
