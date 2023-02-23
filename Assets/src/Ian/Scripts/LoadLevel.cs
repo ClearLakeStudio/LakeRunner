d93a6064098acfcb08528729bbc3336f93bac6ad
im-jking <jamesianking@hotmail.com>
2023-02-22 23:53:02 -0800

    IK - More LoadLevel fixes

--- Assets/src/Ian/Scripts/LoadLevel.cs
+++ Assets/src/Ian/Scripts/LoadLevel.cs
@@ -6,13 +6,13 @@ using UnityEngine;
 public class LoadLevel : MonoBehaviour{
     private float chunkTime;
 
-    //Create timer to check whether a chunk has been loaded in the past two seconds. This prevents framerate-dependent chunk loading.
     void Start(){
+        //Create timer to check whether a chunk has been loaded in the past two seconds. This prevents framerate-dependent chunk loading.
         chunkTime = 0.0f;
     }
 
-    //Subtract the seconds since last fram from the chunk loading timer. If this puts the timer below 0, set it to 0.
     void Update(){
+        //Subtract the seconds since last fram from the chunk loading timer. If this puts the timer below 0, set it to 0.
         if(chunkTime > 0){
             chunkTime -= Time.deltaTime;
         }
@@ -26,7 +26,7 @@ public class LoadLevel : MonoBehaviour{
     void CreateNewChunk(Vector3 heroPos) {
         if(heroPos.x < 10){
             Debug.Log("Ian - Load first three chunks at " + heroPos.x + ", " + (heroPos.x + 25) + ", and " + (heroPos.x + 50));
-        } else if(((heroPos.x) % 25) < .1 && chunkTime){ 
+        } else if(((heroPos.x % 25) < .1) && chunkTime){ 
             Debug.Log("Ian - Load next chunk in level at x-value " + heroPos.x +" and y-value " + heroPos.y);
             chunkTime = 2;
         }
