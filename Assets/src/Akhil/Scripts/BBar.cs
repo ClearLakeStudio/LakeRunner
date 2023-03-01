96013da7d1ffff5dee94b2b4f7c0fac556c48be2
Akhil001 <akhilkarri2002@gmail.com>
2023-03-01 00:02:28 -0800

    BBar

--- Assets/src/Akhil/Scripts/BBar.cs
+++ Assets/src/Akhil/Scripts/BBar.cs
@@ -9,15 +9,11 @@ public class BBar : MonoBehaviour {
     public int BBarCount;
     public Text BBarText;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update() {
         BBarText.text = BBarCount.ToString();
         
     }
 }
+
+
+// attach this file to the gamemanager file
