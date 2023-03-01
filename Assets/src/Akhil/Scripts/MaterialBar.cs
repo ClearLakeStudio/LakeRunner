f80ff84c27a5502c42cd6815d3500921664e68bc
Akhil001 <akhilkarri2002@gmail.com>
2023-02-28 20:51:17 -0800

    Health and resource bar

--- Assets/src/Akhil/Scripts/MaterialBar.cs
+++ Assets/src/Akhil/Scripts/MaterialBar.cs
@@ -6,15 +6,15 @@ using UnityEngine.UI;
 public class MaterialBar : MonoBehaviour {
 
     public Slider matSlider;
-    public int maxMat;
+    public int maxMat;        // <<===== init the max material building resource in the player script
 
     public void SetMaxMat(int maxMat){
         matSlider.maxValue = maxMat;
-        matSlider.value = maxMat;
+        matSlider.value = maxMat;            // <<==== call this func in the player script start func 
         Debug.Log("Akhil : Max material resource set");
     }
 
-    public void updateMaterial (int material) {
+    public void updateMaterial (int material) {       // <<==== call this func in material use or update func 
         matSlider.value = material;
         Debug.Log("Akhil : Material resource update");
     }
