1109d4bdc551ff137517f2687a869d2cc9904992
AkhilKarri2002 <akhilkarri2002@gmail.com>
2023-03-01 16:55:47 -0800

    Update BBar.cs

--- Assets/src/Akhil/Scripts/BBar.cs
+++ Assets/src/Akhil/Scripts/BBar.cs
@@ -5,15 +5,17 @@ using UnityEngine.UI;
 
 public class BBar : MonoBehaviour {
 
-
-    public int BBarCount;
+    public int BBarCount = 0;
     public Text BBarText;
 
     void Update() {
         BBarText.text = BBarCount.ToString();
-        
+    }
+    public void pickUpBBar() {       // <<---- call these func in the item collectible script 
+        BBarCount++;
+    }
+    public void useBBar() {          
+        BBarCount--;
     }
 }
-
-
 // attach this script to the gamemanager script and then attach the textObject to this script
