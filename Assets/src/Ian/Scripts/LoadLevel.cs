fda449801f0b7f5f4d25c31d9a57d6d0cb04c362
im-jking <jamesianking@hotmail.com>
2023-03-02 01:06:14 -0800

    IK - offset and chunkTime fixes

--- Assets/src/Ian/Scripts/LoadLevel.cs
+++ Assets/src/Ian/Scripts/LoadLevel.cs
@@ -25,15 +25,16 @@ public class LoadLevel : MonoBehaviour{
     /*  This function will spawn the first three chunks of the level immediately. It will then check whether 
         the hero has reached the end of a chunk and, if so, spawn another chunk past the current final chunk. */
     public void CreateNewChunk(Vector3 heroPos) {
-        if(heroPos.x < 1 && chunkTime == 0){
+        int offset = 2;
+        if(heroPos.x < .1 && chunkTime == 0){
             Instantiate(platform,new Vector2(heroPos.x, heroPos.y), Quaternion.identity);
-            Instantiate(platform,new Vector2(heroPos.x + 25, heroPos.y), Quaternion.identity);
-            Instantiate(platform,new Vector2(heroPos.x + 50, heroPos.y), Quaternion.identity);
-            Debug.Log("Ian - Load first three chunks at " + heroPos.x + ", " + (heroPos.x + 25) + ", and " + (heroPos.x + 50));
+            Instantiate(platform,new Vector2(heroPos.x + offset, heroPos.y), Quaternion.identity);
+            Instantiate(platform,new Vector2(heroPos.x + 2 * offset, heroPos.y), Quaternion.identity);
+            Debug.Log("Ian - Load first three chunks at " + heroPos.x + ", " + (heroPos.x + offset) + ", and " + (heroPos.x + 2 * offset));
             chunkTime = 100;
         } else if(((heroPos.x % 25) < .1) && chunkTime == 0){ 
-            Instantiate(platform,new Vector2(heroPos.x + 50 + (heroPos.x%25), heroPos.y), Quaternion.identity);
-            Debug.Log("Ian - Load next chunk in level at x-value " + (heroPos.x + 50 + (heroPos.x%25)) +" and y-value " + heroPos.y);
+            Instantiate(platform,new Vector2(heroPos.x + 2*offset + (heroPos.x%25), heroPos.y), Quaternion.identity);
+            Debug.Log("Ian - Load next chunk in level at x-value " + (heroPos.x + 2*offset + (heroPos.x%25)) +" and y-value " + heroPos.y);
             chunkTime = 100;
         }
     }
