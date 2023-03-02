6895e77fb4c372ad7fce7b7673cd0d9baa27a86a
Zaiden Espe <zekraim@gmail.com>
2023-03-02 02:06:21 -0800

    MVP setup

--- Assets/src/Zaiden/GameManager.cs
+++ Assets/src/Zaiden/GameManager.cs
@@ -12,23 +12,47 @@ public class GameManager : MonoBehaviour
 {
     // store references to gameObjects to initialize
     public GameObject platManager;
+    public GameObject chunkManager;
+    public GameObject hero;
+    public GameObject healthBar;
+    public GameObject matBar;
+    public GameObject sunscreenBar;
+    //public GameObject autoPlat; // reference to automatically-made platforms (need a better naming convention for later)
 
     // store references to scripts
     PlatformManager pM;
+    LoadLevel cM;
+    HealthBar hB;
+    MaterialBar mB;
+    SunSbar ssB; // would like to have SunScreen edited to include bar at the end
 
     // store moues data value
     Vector3 mousePos; // stores position of mouse at critical points
     Vector3[] boxCorners;
     bool mouseHold; // whether or not mouse is currently being held
 
+    // store hero position value
+    Vector3 heroPos;
+
     // Start is called before the first frame update
     void Start()
     {
         boxCorners = new Vector3[2];
         mouseHold = false;
-        pM = platManager.GetComponent<PlatformManager>(); // assign script reference
 
-        // instantiate game objects here
+        heroPos = new Vector3();
+
+        pM = platManager.GetComponent<PlatformManager>(); // assign script reference in platformmanager.cs
+        cM = chunkManager.GetComponent<LoadLevel>(); // assign script reference in loadlevel.cs
+        hB = healthBar.GetComponent<HealthBar>(); // assign script reference in HealthBar.cs
+        mB = matBar.GetComponent<MaterialBar>(); // assign script reference in MatBar.cs
+        ssB = sunscreenBar.GetComponent<SunSbar>(); // assign script reference in SunScreen.cs
+
+        // initial calls, will be removed later and moved to hero script
+        hB.SetMaxHealth(10);
+        mB.SetMaxMat(100);
+        ssB.SetSunS(10);
+
     }
 
     // Update is called once per frame
@@ -54,5 +78,13 @@ public class GameManager : MonoBehaviour
                 pM.MakePlat(boxCorners, 0); 
             }
         }
+
+        // update hero position
+        heroPos = hero.transform.position;
+
+        // call chunk creation function
+        cM.CreateNewChunk(new Vector3(heroPos.x, -4.25f, heroPos.z));
+        
+
     }
 }
