9661cb1e184fd1d3815e0c0b192324c8dfd15f14
Akhil001 <akhilkarri2002@gmail.com>
2023-03-01 18:10:03 -0800

    slipper script rename

--- Assets/src/Akhil/Scripts/SlipperHUD.cs
+++ Assets/src/Akhil/Scripts/SlipperHUD.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Slippers : MonoBehaviour
+public class SlipperHUD : MonoBehaviour
 {
     
     public int SlprCount = 0;
