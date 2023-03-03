11f3bdd3b9d94770f6ab0a044c93c23875d7d0e9
im-jking <jamesianking@hotmail.com>
2023-03-02 19:19:11 -0800

    IK - Fixed platform generation

--- Assets/src/Ian/Scripts/LoadLevel.cs
+++ Assets/src/Ian/Scripts/LoadLevel.cs
@@ -12,7 +12,7 @@ public class LoadLevel : MonoBehaviour{
     void Start() {
         //Create timer to check whether a chunk has been loaded in the past two seconds. This prevents framerate-dependent chunk loading.
         chunkTime = 0.0f;
-        platLength = 3;
+        platLength = 5.12f;
         lastPlatformLoc = new Vector2(-1,-1);
         for(int i = 0; i < 20; i++){
                 lastPlatformLoc = new Vector2(lastPlatformLoc.x + platLength, lastPlatformLoc.y);
@@ -20,8 +20,8 @@ public class LoadLevel : MonoBehaviour{
             }
     }
 
-    void FixedUpdate() {
-        //Subtract the seconds since last fram from the chunk loading timer. If this puts the timer below 0, set it to 0.
+    void Update() {
+        //Subtract the seconds since last frame from the chunk loading timer. If this puts the timer below 0, set it to 0.
         if(chunkTime > 0){
             chunkTime--;
         }
@@ -33,11 +33,12 @@ public class LoadLevel : MonoBehaviour{
     /*  This function will spawn the first three chunks of the level immediately. It will then check whether 
         the hero has reached the end of a chunk and, if so, spawn another chunk past the current final chunk. */
     public void CreateNewChunk(Vector3 heroPos) {
-        if(((heroPos.x % platLength) < .05) && chunkTime == 0){ 
+        //if(((heroPos.x % platLength) < .1) && chunkTime == 0){ 
+        if(lastPlatformLoc.x - heroPos.x < (5 * platLength)){  
             lastPlatformLoc = new Vector2(lastPlatformLoc.x + platLength, lastPlatformLoc.y);
             Instantiate(platform, lastPlatformLoc, Quaternion.identity);
             Debug.Log("Ian - Load next chunk in level at x-value " + (lastPlatformLoc.x + platLength) +" and y-value " + lastPlatformLoc.y);
-            chunkTime = 100;
+            chunkTime = 5;
         }
     }
 }
