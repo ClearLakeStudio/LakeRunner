bf0632291d45e0b7f5a16ef829aff12fbb7cc1fc
Logan Finley <finley4.logan@gmail.com>
2023-03-06 11:55:00 -0800

    LF Add stress test script and scene

---
+++ Assets/tst/Logan/StressTest.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+// Spawn objects until a certain threshold is passed
+// selectable thresholds:
+//      fps < 30 five times
+//      cpu utilization > 80% five times within one minute
+//      until blocks push through the walls of the container
+
+public class StressTest : MonoBehaviour
+{
+}
