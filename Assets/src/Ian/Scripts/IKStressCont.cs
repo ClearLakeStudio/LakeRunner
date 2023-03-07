7ff1b5e53746c8a365c1fe1b63d8c14f97f08881
im-jking <jamesianking@hotmail.com>
2023-03-06 23:52:25 -0800

    IK - Finish stress test

--- Assets/src/Ian/Scripts/IKStressCont.cs
+++ Assets/src/Ian/Scripts/IKStressCont.cs
@@ -9,6 +9,11 @@ public class IKStressCont : MonoBehaviour
     private float timer;
     private float fps;
     private Vector3 heroPos;
+    private float frameCount;
+    private float timeCount;
+    private float lastFramerate;
+    private float refreshTime;
+    private int fpsDrop;
 
     // Start is called before the first frame update
     void Start()
@@ -16,12 +21,32 @@ public class IKStressCont : MonoBehaviour
         chunkMan = GameObject.Find("ChunkManager");
         timer = 0;
         heroPos = new Vector3(0,0,0);
+        frameCount = 0;
+        timeCount = 0.0f;
+        lastFramerate = 0.0f;
+        refreshTime = 0.5f;
+        fpsDrop = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        fps = 1.0f / Time.deltaTime;
+        if(timeCount < refreshTime)
+        {
+            timeCount += Time.deltaTime;
+            frameCount++;
+        }
+        else
+        {
+            lastFramerate = (float)frameCount/timeCount;
+            frameCount = 0;
+            timeCount = 0.0f;
+        }
+        fps = lastFramerate;
+        if(fps < 300 && timer == 0)
+            fpsDrop++;
+        if(fpsDrop >= 5)
+            Time.timeScale = 0;
         timer += Time.deltaTime;
         if(timer >= interval){
             chunkMan.GetComponent<LoadLevel>().SendMessage("CreateRandomChunk",heroPos);
@@ -32,5 +57,7 @@ public class IKStressCont : MonoBehaviour
     void OnGUI(){
         GUI.Label(new Rect(Screen.width/8, Screen.height / 10, 300, 300), "FRAME RATE = " + fps.ToString()); 
         GUI.Label(new Rect(Screen.width - (Screen.width/4), Screen.height / 10, 300, 300), "INTERVAL = " + interval);
+        GUI.Label(new Rect(Screen.width - (Screen.width/4), Screen.height - (Screen.height / 10), 300, 300), "DROPS BELOW 300 fps = " + fpsDrop);
+
     }
 }
