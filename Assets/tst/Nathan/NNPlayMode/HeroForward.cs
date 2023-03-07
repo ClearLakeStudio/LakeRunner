d1c5e774e6d712f79ba28f785d20454f84dff9bf
Ledge-Dragmire <nguy10210@gmail.com>
2023-03-07 01:35:23 -0800

    unit tests and hero default prefab update

---
+++ Assets/tst/Nathan/NNPlayMode/HeroForward.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+
+public class HeroForward
+{
+    [UnityTest]
+    public IEnumerator RunDir()
+    {
+        //SceneManager.LoadScene("GameScene");
+        GameObject obj = GameObject.FindWithTag("Hero");
+        yield return new WaitForSeconds(1); //1 second is a timely majic number
+        Assert.IsTrue(0<obj.transform.position.x);
+    }
+}
