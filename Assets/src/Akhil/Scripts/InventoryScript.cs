d34006074fa2f358d48bb098e01db01ad90af9b3
Akhil001 <akhilkarri2002@gmail.com>
2023-04-08 01:24:04 -0700

    Inventory Script

---
+++ Assets/src/Akhil/Scripts/InventoryScript.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryScript : MonoBehaviour
+{
+
+    [SerializeField] int max_items = 5;
+    public int BBarCount;
+    public Text BBarText;
+    public int SGlassCount;     
+    public Text SGlassText;
+    public int SlprCount;
+    public Text SlprText;
+
+    bool addItem(Item.Collected == true)
+    {
+        if (Item.Collected.type == "BrainBlastBar" ){
+            if (BBarCount < max_items)
+            {
+                BBarCount++;
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        if (Item.Collected.type == "Sunglasses" )
+        {
+            if (SGlassCount < max_items)
+            {
+                SGlassCount++;
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        if (Item.Collected.type == "Slippers" )
+        {
+            if (SlprCount < max_items)
+            {
+                SlprCount++;
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+
+    bool removeItem (string type)
+    {
+        if (type == "BrainBlastBar")
+        {
+            if (BBarCount>0)
+            {
+                BBarCount--;
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+        }
+        if (type == "Sunglasses")
+        {
+            if (SGlassCount>0)
+            {
+                SGlassCount--;
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+        }
+        if (type == "Slippers")
+        {
+            if (SlprCount>0)
+            {
+                SlprCount--;
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+        }
+    }
+
+    void Start()
+    {
+        BBarCount = 0;
+        SGlassCount = 0;     
+        SlprCount = 0;
+    }
+
+    void Update()
+    {
+        BBarText.text = BBarCount.ToString();
+        SGlassText.text = SGlassCount.ToString();
+        SlprText.text = SlprCount.ToString();
+    }
+}
