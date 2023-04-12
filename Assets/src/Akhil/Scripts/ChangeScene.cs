6f1cfcf2947ab8cf94a8ec6d05930806d36c559a
Akhil001 <akhilkarri2002@gmail.com>
2023-04-11 21:16:56 -0700

    Update ChangeScene.cs

--- Assets/src/Akhil/Scripts/ChangeScene.cs
+++ Assets/src/Akhil/Scripts/ChangeScene.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ChangeScene : MonoBehaviour {
+public class ChangeScene : MonoBehaviour 
+{
+  
   public GameObject gameOverUI;
   bool finishGame = false;
 
-  DisableNextLevelButton d = new DisableNextLevelButton();
-  DisableNextLevelButton e = new EnableNextLevelButton();
+  public DisableNextLevelButton d;
+  public DisableNextLevelButton e;
 
   //make an object of this script in the gamemanager or player script and attach gameoverScreen to it.
   // for game over screen call this func like this when the player finishes
