13ee497b689f72f96a347f4447d2a1c73e8efc77
im-jking <jamesianking@hotmail.com>
2023-04-19 09:33:57 -0700

    IK - Update Akhil function calls

--- Assets/src/Ian/Scripts/LevelFinish.cs
+++ Assets/src/Ian/Scripts/LevelFinish.cs
@@ -20,7 +20,7 @@ public class LevelFinish : MonoBehaviour
     private Vector2 rayLoc;
     private bool platforms;
 
-    void Start(){
+    void Awake(){
         rayLoc = new Vector2(transform.position.x + transform.localScale.x/2, transform.position.y);
         platMan = GameObject.Find("UserPlatformManager");
         platforms = true;
@@ -42,8 +42,8 @@ public class LevelFinish : MonoBehaviour
         foreach(RaycastHit2D hit in colls){
             if(hit.collider.gameObject.tag == "Hero"){
                 platMan.SetActive(false);
-                gameOver.GetComponent<ChangeScene>().gameOver();
-                gameOver.GetComponent<ChangeScene>().finishGame = true;
+                gameOver.GetComponent<ChangeScene>().GameOver();
+                gameOver.GetComponent<ChangeScene>().FinishGame = true;
                 platforms = false;
                 levData.FinishLevel(SceneManager.GetActiveScene().name);
             }
@@ -52,8 +52,8 @@ public class LevelFinish : MonoBehaviour
         foreach(RaycastHit2D hit in colls){
             if(hit.collider.gameObject.tag == "Hero"){
                 platMan.SetActive(false);
-                gameOver.GetComponent<ChangeScene>().gameOver();
-                gameOver.GetComponent<ChangeScene>().finishGame = true;
+                gameOver.GetComponent<ChangeScene>().GameOver();
+                gameOver.GetComponent<ChangeScene>().FinishGame = true;
                 platforms = false;
                 levData.FinishLevel(SceneManager.GetActiveScene().name);
             }
